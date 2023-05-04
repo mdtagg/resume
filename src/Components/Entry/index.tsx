cd2a0d0cc640dcadef4c85459b8ee03b7cd4148e
Mike Tagg <mdtagg91@gmail.com>
2023-05-03 18:19:03 -0700

    Entry component added
    entry component was changed to a template for background entries

--- src/Components/Entry/index.tsx
+++ src/Components/Entry/index.tsx
@@ -1,22 +1,37 @@
 
+interface EntryProps {
+    dateStart:string 
+    dateEnd:string 
+    title:string 
+    company?:string 
+    summary:string[]
+}
 
-const Entry = () => {
+const Entry = (props:EntryProps) => {
+
+    const { dateStart,dateEnd,title,company,summary } = props
+
     return (
-        <div className="flex text-[.6rem] gap-1 mx-6 ">
-            <div className="flex flex-col font-bold">
-                <p>JAN 2018</p>
-                <p>JAN 2021</p>
+        <div className="flex  text-[.6rem] ml-5 ">
+            <div className="flex flex-col font-bold w-1/5">
+                <p>{dateStart}</p>
+                <p>{dateEnd}</p>
             </div>
-            <div className="h-full relative z-10 w-1/3 flex flex-col justify-start items-center">
+            <div className="h-full relative z-10 w-1/5 flex flex-col justify-start items-center">
                 <div className="h-4 w-4 rounded-full border border-black"></div>
                 <div className="h-full border border-black"></div>
             </div>
-            <div className="font-bold w-1/3 flex flex-col gap-2">
-                <p>SALES ASSOCIATE</p>
-                <p>REI</p>
-                <ul className="text-[.5rem] font-normal list-disc">
-                    <li>Leveraged rock climbing skills to inform customers on gear and technique</li>
-                    <li>Top in Sales</li>
+            <div className="font-bold w-3/5 flex flex-col gap-1">
+                <p className="text-[.5rem]">{title}</p>
+                {company &&
+                    <p className="text-[.5rem]">
+                        {company}
+                    </p>
+                }
+                <ul className="flex flex-col text-[.5rem] font-normal list-disc w-full gap-1">
+                    {summary.map((entry) => {
+                        return <li>{entry}</li>
+                    })}
                 </ul>
             </div>
         </div>
--- src/Components/Experience/index.tsx
+++ src/Components/Experience/index.tsx
@@ -2,12 +2,59 @@ import { SectionTitle } from "../SectionTitle";
 import { Entry } from "../Entry";
 
 const Experience =  () => {
+
+    const summaries = {
+        "weatherApp":
+        [
+            "Built with React and Tailwind, later converted to Typescript",
+            "Processes data from APIs, gives users information relating to rock types and the weather",
+            "Uses a variety of React hooks, both built in and custom",
+            "Dynamically renders layouts based on user input"
+        ],
+        "REI": 
+        [
+            "Consistently in top sales ranks",
+            "Trained most areas of the store within 2 months"
+        ],
+        "EDUCATION":
+        [
+            "Wrote shipping route optimization modeling programs in LINGO",
+            "Frequently worked within multi-cultural teams to present research",
+            "Consulted with a local business owner to help develop operational strategies"
+        ]
+    }
+
     return ( 
-        <div className="w-full h-full flex flex-col gap-2">
+        <div className="w-full h-full flex flex-col gap-1 mt-2">
+            <SectionTitle
+                title="RECENT PROJECTS"
+            />
+            <Entry
+                dateStart="FEB 2023"
+                dateEnd="APR 2023"
+                title="Rock Climbing Weather App"
+                summary={summaries.weatherApp}
+            />
             <SectionTitle
                 title="WORK EXPERIENCE"
             />
-            <Entry/>
+            <Entry
+                dateStart="JAN 2019"
+                dateEnd="JAN 2022"
+                title="Sales Associate"
+                company="REI"
+                summary={summaries.REI}
+            />
+            <SectionTitle
+                title="EDUCATION"
+            />
+            <Entry
+                dateStart="SEP 2016"
+                dateEnd="AUG 2018"
+                title="MBA (Masters of Business Administration)"
+                company="California Lutheran University"
+                summary={summaries.EDUCATION}
+            />
         </div>
      );
 }
